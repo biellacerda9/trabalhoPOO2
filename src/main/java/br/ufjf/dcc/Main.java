9e483642628f6896f21d13f3318a65d18fa87b56
JG-Souza <joaogsouzafma@gmail.com>
2026-01-16 17:47:15 -0300

    Implementa o menu de investidores

--- src/main/java/br/ufjf/dcc/Main.java
+++ src/main/java/br/ufjf/dcc/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
         while(true){
             println("1. Menu de Ativos");
-            println("2. Menu de Investidor");
+            println("2. Menu de Investidores");
             println("3. Encerrar");
             print("Escolha uma opção: ");
             String escolha = scanner.nextLine();
@@ -21,7 +21,7 @@ public class Main {
             if (escolha.equals("1")) {
                 exibeMenuAtivos();
             } else if (escolha.equals("2")) {
-                println("Entrou no 2.");
+                exibeMenuDeInvestidores();
             } else if (escolha.equals("3")) {
                 break;
             } else {
@@ -40,6 +40,38 @@ public class Main {
             println("3. Editar ativo");
             println("4. Excluir ativo");
             println("5. Exibir relatório de ativos");
+            println("6. Voltar");
+            print("Escolha uma opção: ");
+            String escolha = scanner.nextLine();
+
+            if (escolha.equals("1")) {
+                println("Entrou no 1.");
+            } else if (escolha.equals("2")) {
+                println("Entrou no 2.");
+            } else if (escolha.equals("3")) {
+                println("Entrou no 3.");
+            } else if (escolha.equals("4")) {
+                println("Entrou no 4.");
+            } else if (escolha.equals("5")) {
+                println("Entrou no 5.");
+            } else if (escolha.equals("6")) {
+                break;
+            } else {
+                println("Opção inválida. Tente novamente.");
+            }
+        }
+    }
+
+    public static void exibeMenuDeInvestidores() {
+        Scanner scanner = new Scanner(System.in);
+        println("Esse é o menu de investidores!");
+
+        while(true){
+            println("1. Cadastrar investidor");
+            println("2. Cadastrar investidor em lote");
+            println("3. Exibir todos investidores");
+            println("4. Excluir investidores");
+            println("5. Selecionar investidor");
             println("6. Voltar");
             print("Escolha uma opção: ");
             String escolha = scanner.nextLine();
