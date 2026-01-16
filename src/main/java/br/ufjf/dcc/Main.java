3439155e6094dc0efae1fc3adf80ba3562afa107
JG-Souza <joaogsouzafma@gmail.com>
2026-01-16 18:57:55 -0300

    Implementa função para leitura de arquivos

--- src/main/java/br/ufjf/dcc/Main.java
+++ src/main/java/br/ufjf/dcc/Main.java
@@ -1,6 +1,14 @@
 package br.ufjf.dcc;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     static void main() {
@@ -141,6 +149,20 @@ public class Main {
             } else {
                 println("Opção inválida. Tente novamente.");
             }
+        }
+    }
+
+    // ToDo: Passar esse trecho (e suas importações) para uma classe própria depois
+    public static List<String[]> lerCSV(String nomeArquivo) {
+        Path caminho = Paths.get(nomeArquivo);
+        try (Stream<String> linhas = Files.lines(caminho)) { // linhas armazena o fluxo de strings, onde cada string é uma linha do arquivo
+            return linhas
+                    .skip(1) // Pula o cabeçalho
+                    .map(linha -> linha.split(",")) // Quebra a linha do registro em um array de strings, separados por virgulas
+                    .collect(Collectors.toList()); // Pega todos os arrays e coloca dentro de uma lista
+        } catch (IOException e) { // Se o arquivo não for encontrado na raiz
+            System.err.println("ERRO: Não foi possível ler o arquivo: " + nomeArquivo);
+            return Collections.emptyList();
         }
     }
 
